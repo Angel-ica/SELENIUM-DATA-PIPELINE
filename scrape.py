1492e339f443d28aedb7f99b61e1e7d3f48a50c9
Angelica Aluo <angelicaaluo@Angelicas-MacBook-Air.local>
2022-11-11 01:19:30 +0000

    updates files

--- scrape.py
+++ scrape.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import selenium.common.exceptions
 import pandas as pd
 import time
 import urllib.request
+import os.path
 
 class WebCrawl():
     def __init__(self):
@@ -19,8 +21,7 @@ class WebCrawl():
     def accept_cookies(self):
         accept_cookies=self.driver.find_element(by=By.XPATH, value ='.//button[@id="onetrust-accept-btn-handler"]')
         accept_cookies.click()
-        # cookie=self.driver.get_cookie
-        # print(cookie)
+        
     def redirect_to_vegan_page(self):
         search = self.driver.find_element(by=By.XPATH, value='.//form[@class="hd-search__form hd-searchProminent__form"]')
         search.click()
@@ -34,38 +35,37 @@ class WebCrawl():
     def scroll_down(self):
         x=0
         page = self.driver.find_element(by=By.TAG_NAME, value='body')
+        load_more = self.driver.find_element(by=By.XPATH, value='.//button[@class="btn-primary show-more"]')
         while True:
+            #time.sleep(3)
             page.send_keys(Keys.PAGE_DOWN)
-            time.sleep(1)
+            time.sleep(1) 
             x+=1
-            if x==55:
-                print("55 scrolls")
+            if load_more:
+                try:
+                    load_more.click()
+                    time.sleep(2)
+                
+                except selenium.common.exceptions.StaleElementReferenceException:pass
                 break
-        # self.driver.implicitly_wait(30)
-        # time.sleep(20)
-        # self.driver.execute_script("window.scrollTo({left: 0, top: document.body.scrollHeight, behavior : 'smooth'});")
+            else:
+                self.get_vegan_items()
+                break
 
 
     def remove_promo(self):
-        promo=self.driver.find_element(by=By.XPATH, value="//span[@id='aq-promo-close']")
-        promo.click()
-         # if promo:
-        #     promo.click()
-        # else:
-        #     print('id didnt work')
-                      
-    
+        self.driver.find_element(by=By.XPATH, value=("//span[@id='aq-promo-close']/*[name()='svg']/*[name()='rect']")).click()
+
     def get_img_logo(self):
             logo=self.driver.find_element(by=By.XPATH, value="//img[@class='hd-header__logoImg']")
             img=logo.get_attribute("src")
             print(img)
             urllib.request.urlretrieve(str(img),"sample_data.jpg")
-            #assert "src" in logo
             
-    def get_vegan_items(self):
-            products=self.driver.find_elements(by=By.XPATH,value= "//ul[@class='fops fops-regular fops-shelf']//li")
+    def get_vegan_items(self)-> dict :
+            self.products=self.driver.find_elements(by=By.XPATH,value= "//ul[@class='fops fops-regular fops-shelf']//li")
             self.data_dict={"all_name":[],"all_price":[],"all_url":[],"all_prod_img":[]}
-            for product in products:
+            for product in self.products:
                 name=product.find_element(by=By.XPATH,value=".//h4[@class='fop-title']/span")
                 self.data_dict["all_name"].append(name.text)
                 price=product.find_element(By.CLASS_NAME,("fop-price"))
@@ -73,11 +73,16 @@ class WebCrawl():
                 url=product.find_element(by=By.XPATH, value=".//div[@class='fop-contentWrapper']/a")
                 product_link=url.get_attribute("href")
                 self.data_dict["all_url"].append(product_link)
-                img=product.find_element(By.CLASS_NAME,("fop-img"))
-                prod_img=img.get_attribute("src")
-                self.data_dict["all_prod_img"].append(prod_img)
-                return self.data_dict
-            self.driver.quit()
+                img=product.find_element(by=By.XPATH, value=".//div[@class='fop-contentWrapper']//a//img")
+                self.prod_img=img.get_attribute("src")
+                self.prod_img_link=('https://www.ocado.com'+self.prod_img)
+                self.data_dict["all_prod_img"].append(self.prod_img_link)
+                
+                self.prod_img_path=urllib.request.urlretrieve(self.prod_img_link,f"image_{name}.jpg")
+                #print(self.data_dict)
+
+                
+            #self.driver.quit()
 
     def store_in_csv(self):
         df=pd.DataFrame(self.data_dict)
@@ -97,6 +102,7 @@ def run():
     crawl.get_page()
     crawl.accept_cookies()
     crawl.get_img_logo()
+    #crawl.remove_promo()
     crawl.redirect_to_vegan_page()
     crawl.scroll_down()
     crawl.get_vegan_items()
--- test_scrape.py
+++ test_scrape.py
@@ -13,41 +13,22 @@ class TestScrape(unittest.TestCase):
 
     def setUp(self):
         self.scraper = wb()
-        self.get_page=self.scraper.get_page()
-        
+        self.scraper.get_page()
+        self.scraper.accept_cookies()
 
-    def test__get_page(self):
-        #get_page=self.scraper.get_page()
-        page_url=self.scraper.driver.current_url
-        self.assert_("ocado",self.get_page)
-        self.assert_(page_url,self.get_page)
-
-    def test_accept_cookies(self):
-        print('craving cookies')
-        accept_cookies=self.scraper.accept_cookies()
-
-        time.sleep(3)
-        #assert not self.accept_cookies
-        cookies=None
-        self.assertEqual(type(accept_cookies),type(cookies))
-        print('no more cookies!')
-
+    
     def test_redirect_to_vegan_page(self):
-        self.get_page=self.scraper.get_page()
-        self.accept_cookies=self.scraper.accept_cookies()
         vegan_page = self.scraper.redirect_to_vegan_page()
-        time.sleep(1)
         get_url = self.scraper.driver.current_url
         self.assert_(get_url,vegan_page)
 
 
     def test_img_logo(self):
-        time.sleep(2)
         self.assert_(os.path.isfile('./sample_data.jpg')
 )  
     def test_vegan_items(self):
-        length_vegan_items={}
-        self.assertEqual(type(length_vegan_items),type(self.scraper.get_vegan_items()))
+        vegan_items={}
+        self.assertEqual(type(vegan_items),type(self.scraper.get_vegan_items()))
 
     def test_to_csv(self):
         self.assert_(os.path.isfile('./my_file.csv'))
